444feca634e280fe77e12e0560423435ead61c20
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-10 17:45:47 +0500

    filed types and options

--- api/models.py
+++ api/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+quality_choices = (
+    ('n', "New"),
+    ('o', "Old")
+)
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    price = models.FloatField(default=00.00)
+    quantity = models.PositiveIntegerField(default=0)
+    is_there = models.BooleanField(default=True)
+    quality = models.CharField(max_length=20, choices=quality_choices, default='New')
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
